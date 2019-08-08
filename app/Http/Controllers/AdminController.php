16518630207dc48d9a69832ac0d6d49034bd9a6b
Lemontrash <Jakartasdedibiyb0ss3>
2019-08-08 15:33:42 +0300

    Big commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -26,7 +26,7 @@ class AdminController extends Controller
     }
 
     public function deleteCategory(Request $request){
-        $id = $request->get('id');
+        $id = $request->geta('id');
         $category = Category::find($id);
 
         if(empty($category)){
